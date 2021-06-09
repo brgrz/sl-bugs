f958274cbf5c7ac1b6219b0a0c35447088488555
brgrz <marko@centiva.si>
2021-06-09 22:46:57 +0200

    svelte:head not working in sveltekit for some reason

--- handler-violations/src/routes/index.svelte
+++ handler-violations/src/routes/index.svelte
@@ -41,13 +41,3 @@
 </sl-drawer>
 
 <sl-button bind:this={openButton}>Open Drawer</sl-button>
-
-<!-- <svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.43/dist/themes/base.css"
-	/>
-	<script
-		type="module"
-		src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.43/dist/shoelace.js"></script>
-</svelte:head> -->
