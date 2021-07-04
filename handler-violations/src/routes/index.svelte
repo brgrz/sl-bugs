9e7bb325baf5dd0e8d580c365732314bdeaecf8c
brgrz <marko@centiva.si>
2021-07-04 09:03:17 +0200

    updated for new bug

--- handler-violations/src/routes/index.svelte
+++ handler-violations/src/routes/index.svelte
@@ -19,25 +19,30 @@
 <sl-drawer label="Drawer" class="drawer-overview">
 	<sl-tab-group>
 		<sl-tab slot="nav" panel="general">General</sl-tab>
-		<sl-tab slot="nav" panel="custom">Custom</sl-tab>
-		<sl-tab slot="nav" panel="advanced">Advanced</sl-tab>
-		<sl-tab slot="nav" panel="disabled" disabled>Disabled</sl-tab>
-
 		<sl-tab-panel name="general">
-			<sl-input placeholder="Type something" value={name} />
-			<br />
-			<sl-select placeholder="Select one" value={option}>
-				<sl-menu-item value="option-1">Option 1</sl-menu-item>
-				<sl-menu-item value="option-2">Option 2</sl-menu-item>
-				<sl-menu-item value="option-3">Option 3</sl-menu-item>
-			</sl-select>
+			<div class="grid">
+				<sl-input placeholder="Type something" value={name} />
+				<sl-select placeholder="Select one" value={option}>
+					<sl-menu-item value="option-1">Option 1</sl-menu-item>
+					<sl-menu-item value="option-2">Option 2</sl-menu-item>
+					<sl-menu-item value="option-3">Option 3</sl-menu-item>
+				</sl-select>
+				<sl-input name="size" type="text" label="Test" placeholder="placeholder" required="true">
+					<div slot="help-text">help text not selectable</div>
+				</sl-input>
+			</div>
 		</sl-tab-panel>
-		<sl-tab-panel name="custom">This is the custom tab panel.</sl-tab-panel>
-		<sl-tab-panel name="advanced">This is the advanced tab panel.</sl-tab-panel>
-		<sl-tab-panel name="disabled">This is a disabled tab panel.</sl-tab-panel>
 	</sl-tab-group>
 
 	<sl-button slot="footer" type="primary" bind:this={closeButton}>Close</sl-button>
 </sl-drawer>
 
 <sl-button bind:this={openButton}>Open Drawer</sl-button>
+
+<style>
+	.grid {
+		display: grid;
+		grid-auto-flow: row;
+		grid-gap: 2rem;
+	}
+</style>
